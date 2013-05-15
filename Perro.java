c55d06894e6bc253ab70d9600362aea9edd0a767
Lope Monedero Moreno <cscyclopssc@hotmail.com>
2013-05-15 23:56:47 +0200

    segunda version
    añadiendo comentarios

--- Perro.java
+++ Perro.java
@@ -4,8 +4,7 @@
  * 		@author Lope
  *		@version 1.0
  */
- //probando comentario
- //tercer comentario
+//comentario primero
 public class Perro {
 		/**
 		*	Campos de la clase Perro: peso,nombre,raza, patas, collar, contador
